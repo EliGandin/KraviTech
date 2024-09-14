ed1c89a8eea323e9cc6d56f1c77e5dafc57d6f68
EliGandin <git>
2024-09-14 19:36:17 +0300

    setup backend

---
+++ server/globals/enums/experience_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Experience(Enum):
+    LOW = "low"
+    MID = "mid"
+    HIGH = "high"
---
+++ server/globals/enums/field_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Field(Enum):
+    DATA = "data"
+    HARDWARE = "hardware"
+    SOFTWARE = "software"
