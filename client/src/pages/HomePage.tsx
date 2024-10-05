0a0b94fad2325ed268e0673672fc9e03782c686d
EliGandin <git>
2024-10-05 19:13:28 +0300

    styled the home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <div>Image</div>
+
+      <div className="flex flex-col justify-center align-middle">
+        <Button asChild>
+          <Link to="/login">Let's get started</Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
