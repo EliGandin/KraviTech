29cc3b90fcdc7751465d687fdaa3885a41d99a7f
EliGandin <git>
2024-11-03 08:33:28 +0200

    deleted old backend

--- server/globals/enums/role_enum.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Role(Enum):
-    MENTOR = "mentor"
-    MENTI = "menti"
-    ADMIN = "admin"
--- server/globals/validation/validation_result.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-
-
-class ValidationResult:
-    def __init__(self, is_valid: bool, message: Optional[str] = None):
-        self.is_valid = is_valid
-        self.message = message
