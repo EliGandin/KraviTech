b766b07fadab01ef92962b59b031aed36a264f10
EliGandin <git>
2024-12-10 11:51:08 +0200

    added E2E to update profile

---
+++ E2E/util/types.ts
@@ -0,0 +1,11 @@
+export type Mentor = {
+  id: number;
+  name: string;
+  email: string;
+  phone_number: string;
+  company: string;
+  position: string;
+  field: string;
+  experience: string;
+  status: string;
+}
