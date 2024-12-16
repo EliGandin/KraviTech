520fa2b1899e762fa8c9b1a4c73c47da237b8c36
EliGandin <git>
2024-12-16 10:12:54 +0200

    normalized phone number

--- client/src/utils/mappers/signup/mentorSignupMapper.ts
+++ client/src/utils/mappers/signup/mentorSignupMapper.ts
@@ -1,18 +1,13 @@
 import { z } from "zod";
+
 import { MentorSignupFormSchema } from "@/schemas/signup/MentorSignupFormSchema.ts";
 import { IMentorSignup } from "@/global/interfaces/signupInterfaces.ts";
+import { normalizePhoneNumber } from "@/utils/formatters/formatFields.ts";
 
 export const mentorSignupMapper = (
   data: z.infer<typeof MentorSignupFormSchema>,
 ): IMentorSignup => {
-  return {
-    name: data.name,
-    email: data.email,
-    password: data.password,
-    phone_number: data.phoneNumber,
-    position: data.position,
-    company: data.company,
-    field: data.field,
-    experience: data.experience,
-  };
+  const normalizedPhoneNumber = normalizePhoneNumber(data.phoneNumber);
+
+  return { ...data, phone_number: normalizedPhoneNumber };
 };
