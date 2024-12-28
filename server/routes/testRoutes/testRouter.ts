0120bbbf87e13359f4b9e39fcf6695e4650d4ce5
EliGandin <git>
2024-12-28 13:25:12 +0200

    commented not ready code

--- server/routes/testRoutes/testRouter.ts
+++ server/routes/testRoutes/testRouter.ts
@@ -1,24 +1,24 @@
-import { Router } from "express";
-import { StatusCodes } from "http-status-codes";
-
-import { mongo } from "@/db/mongo";
-import db from "@/db/db";
-
-const testRouter = Router();
-
-testRouter.get("/healthcheck", async (req, res) => {
-  try {
-    const sqlDB = db;
-    const mongoDB = await mongo();
-    if (!sqlDB || mongoDB.connection.readyState !== 1) {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("SQL DB is not connected");
-      return;
-    }
-
-    res.status(StatusCodes.OK).send("DBs are connected !!");
-  } catch (err) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
-  }
-});
-
-export default testRouter;
+// import { Router } from "express";
+// import { StatusCodes } from "http-status-codes";
+//
+// import { mongo } from "@/db/mongo";
+// // import db from "@/db/db";
+//
+// const testRouter = Router();
+//
+// testRouter.get("/healthcheck", async (req, res) => {
+//   try {
+//     const sqlDB = db;
+//     const mongoDB = await mongo();
+//     if (!sqlDB || mongoDB.connection.readyState !== 1) {
+//       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("SQL DB is not connected");
+//       return;
+//     }
+//
+//     res.status(StatusCodes.OK).send("DBs are connected !!");
+//   } catch (err) {
+//     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+//   }
+// });
+//
+// export default testRouter;
