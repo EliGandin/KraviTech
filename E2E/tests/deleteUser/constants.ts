f3fca7b0d636cb29c47bb39833e7417fcbd9f171
EliGandin <git>
2025-01-10 20:58:20 +0200

    initialized delete user E2E

---
+++ E2E/tests/deleteUser/constants.ts
@@ -0,0 +1,5 @@
+export const baseURL = "http://localhost:5172";
+export const menti = { id: 1, email: "yuval@test.com", password: "1234" };
+export const mentor = { id: 1, email: "amit@test.com", password: "1234" };
+export const admin = { id: 1, email: "eli@test.com", password: "1234" };
+export const INACTIVE_STATUS = "Inactive";
