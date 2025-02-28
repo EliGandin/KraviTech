61e5421a007037eebe578190aa0027a5d9bdd54f
EliGandin <git>
2025-02-28 22:42:02 +0200

    refactored code post dynamodb migration

--- database/init.sql
+++ database/init.sql
@@ -66,20 +66,6 @@ CREATE TABLE IF NOT EXISTS messages
     operator_id  INTEGER REFERENCES admins (id) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS tasks
-(
-    id               SERIAL PRIMARY KEY,
-    title            VARCHAR NOT NULL,
-    description      VARCHAR NOT NULL,
-    status           task_status DEFAULT 'NEW',
-    created_date     DATE        DEFAULT CURRENT_DATE,
-    in_progress_date DATE        DEFAULT NULL,
-    completed_date   DATE        DEFAULT NULL,
-    menti_id         INTEGER REFERENCES mentis (id) ON DELETE SET NULL,
-    mentor_id        INTEGER REFERENCES mentors (id) ON DELETE SET NULL,
-    sub_tasks        JSONB       DEFAULT '[]'
-);
-
 INSERT INTO mentors (name, email, phone_number, password, field, company, position, experience, status, start_date,
                      end_date)
 VALUES ('Amit Cohen', 'amit@test.com', '0505050050', '$2a$12$0B.EaJM27vxqP7kZbzdbcukfKbVaPKnfyAvggeVWMB8MT/fFEmQMG',
@@ -123,46 +109,3 @@ VALUES ('Yuval Regev', 'yuval@test.com', '0532648574', '$2a$12$0B.EaJM27vxqP7kZb
 INSERT INTO messages (name, email, phone_number, title, message, status, operator_id)
 VALUES ('John Doe', 'john@test.com', '0501234567', 'help', 'I have a question', 'OPEN', 1),
        ('Jane Doe', 'jane@test.com', '0501234567', 'question', 'I have a question', 'OPEN', 1);
-
-INSERT INTO tasks (title, description, status, created_date, in_progress_date, completed_date, menti_id, mentor_id,
-                   sub_tasks)
-VALUES ('Learn Python', 'Learn Python basics', 'NEW', '2021-01-01', null, null, 1, 1, '[
-  {
-    "id": "1_1",
-    "title": "Install Python",
-    "description": "Install Python on your computer",
-    "status": "NEW",
-    "created_date": "2021-01-01",
-    "in_progress_date": null,
-    "completed_date": null
-  },
-  {
-    "id": "1_2",
-    "title": "Learn Python syntax",
-    "description": "Learn the basic syntax of Python",
-    "status": "NEW",
-    "created_date": "2021-01-01",
-    "in_progress_date": null,
-    "completed_date": null
-  }
-]'),
-       ('Learn SQL', 'Learn SQL basics', 'NEW', '2021-01-01', null, null, 2, 1, '[
-         {
-           "id": "2_1",
-           "title": "Install MySQL",
-           "description": "Install MySQL on your computer",
-           "status": "NEW",
-           "created_date": "2021-01-01",
-           "in_progress_date": null,
-           "completed_date": null
-         },
-         {
-           "title": "Learn SQL syntax",
-           "description": "Learn the basic syntax of SQL",
-           "status": "NEW",
-           "created_date": "2021-01-01",
-           "in_progress_date": null,
-           "completed_date": null
-         }
-       ]'),
-       ('Test Task', 'Test Description', 'NEW', '2021-01-01', null, null, 5, 4, '[]');
--- server/globals/types/Task.type.ts
+++ server/globals/types/Task.type.ts
@@ -1,13 +1,15 @@
+import * as crypto from "node:crypto";
+
 export type Task = {
-  id: number;
+  task_id: number | string | crypto.UUID;
   title: string;
   description: string;
   status: string;
-  created_date: Date;
-  in_progress_date: Date;
-  completed_date: Date;
-  menti_id: number;
-  mentor_id?: number;
+  created_date: string;
+  in_progress_date?: string | null;
+  completed_date?: string | null;
+  menti_id: number | string;
+  mentor_id?: number | string;
   sub_tasks_count: number;
 }
 
@@ -16,7 +18,7 @@ export type SubTask = {
   title: string;
   description: string;
   status: string;
-  created_date: Date;
-  in_progress_date: Date;
-  completed_date: Date;
+  created_date: string | null;
+  in_progress_date: string | null;
+  completed_date: string | null;
 }
