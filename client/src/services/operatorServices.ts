dbf9f61be4a1b1ca26586b924cc78400700990e2
EliGandin <87780727+EliGandin@users.noreply.github.com>
2025-04-13 18:29:48 +0300

    129 front url refactor (#129)
    * updated gitignore
    
    * added env variable
    
    * added env variable
    
    * added env variable
    
    ---------
    
    Co-authored-by: EliGandin <git>

--- client/src/services/operatorServices.ts
+++ client/src/services/operatorServices.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 import { Operator } from "@/global/interfaces/Props/MessageProps.ts";
 
+const URL = `${import.meta.env.VITE_BACKEND_URL}/operator`;
+
 export const getAllOperators = async (): Promise<Operator[]> => {
-  const { data } = (await axios.get("http://localhost:8000/operator")).data;
+  const { data } = (await axios.get(`${URL}`)).data;
 
   return data;
 };
--- client/src/services/taskServices.ts
+++ client/src/services/taskServices.ts
@@ -8,16 +8,16 @@ import {
   TaskDetails,
 } from "@/global/interfaces/tasksInterfaces.ts";
 
+const URL = `${import.meta.env.VITE_BACKEND_URL}/tasks`;
+
 export const getTasksByMentor = async (id: number): Promise<MentorTasks[]> => {
-  const { data } = (await axios.get(`http://localhost:8000/tasks/mentor/${id}`))
-    .data;
+  const { data } = (await axios.get(`${URL}/mentor/${id}`)).data;
 
   return data;
 };
 
 export const getTasksByMenti = async (id: number): Promise<MentiTasks[]> => {
-  const { data } = (await axios.get(`http://localhost:8000/tasks/menti/${id}`))
-    .data;
+  const { data } = (await axios.get(`${URL}/menti/${id}`)).data;
 
   return data;
 };
@@ -25,23 +25,21 @@ export const getTasksByMenti = async (id: number): Promise<MentiTasks[]> => {
 export const getTaskDetails = async (
   taskId: string,
 ): Promise<TaskDetails[]> => {
-  const { data } = (
-    await axios.get(`http://localhost:8000/tasks/TaskDetails/${taskId}`)
-  ).data;
+  const { data } = (await axios.get(`${URL}/TaskDetails/${taskId}`)).data;
 
   return data;
 };
 
 export const addTask = (mentiId: string, task: NewTask): Promise<void> => {
-  return axios.post(`http://localhost:8000/tasks/${mentiId}`, task);
+  return axios.post(`${URL}/${mentiId}`, task);
 };
 
 export const addSubtask = async (taskId: string, subtask: Subtask) => {
-  await axios.post(`http://localhost:8000/tasks/SubTask/${taskId}`, subtask);
+  await axios.post(`${URL}/SubTask/${taskId}`, subtask);
 };
 
 export const changeTaskStatus = (id: number, status: string): Promise<void> => {
-  return axios.put(`http://localhost:8000/tasks/ChangeTaskStatus/${id}`, {
+  return axios.put(`${URL}/ChangeTaskStatus/${id}`, {
     status,
   });
 };
@@ -51,7 +49,7 @@ export const changeSubtaskStatus = (
   subtaskId: string,
   status: string,
 ): Promise<void> => {
-  return axios.put(`http://localhost:8000/tasks/ChangeSubtaskStatus/${id}`, {
+  return axios.put(`${URL}/ChangeSubtaskStatus/${id}`, {
     subtaskId,
     status,
   });
